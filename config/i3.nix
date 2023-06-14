cdc85ba7d0108e897bca094d6525bbef0fe114c1
Guillaume Friloux <guillaume@friloux.me>
2023-06-14 21:06:01 +0200

    i3 update

--- config/i3.nix
+++ config/i3.nix
@@ -5,8 +5,8 @@ in {
   home.packages = with pkgs; [
     rofi
     polybar
-    i3lock
     scrot       # Take screenshot for i3lock
+    #i3lock
     mpv         # For my i3lock script
     imagemagick # To create background for i3lock
     xautolock
@@ -32,6 +32,44 @@ in {
         "${mod}+d" = "exec fish -c 'rofi -show drun -theme ~/.config/rofi/launchers/type-7/style-1.rasi'";
       };
       bars = [];
+      colors = {
+        focused = {
+          border      = "#6272A4";
+          background  = "#6272A4";
+          text        = "#F8F8F2";
+          indicator   = "#6272A4";
+          childBorder = "#6272A4";
+        };
+        focusedInactive = {
+          border      = "#44475A";
+          background  = "#44475A";
+          text        = "#F8F8F2";
+          indicator   = "#44475A";
+          childBorder = "#44475A";
+        };
+        unfocused = {
+          border      = "#282A36";
+          background  = "#282A36";
+          text        = "#BFBFBF";
+          indicator   = "#282A36";
+          childBorder = "#282A36";
+        };
+        urgent = {
+          border      = "#44475A";
+          background  = "#FF5555";
+          text        = "#F8F8F2";
+          indicator   = "#FF5555";
+          childBorder = "#FF5555";
+        };
+        placeholder = {
+          border      = "#282A36";
+          background  = "#282A36";
+          text        = "#F8F8F2";
+          indicator   = "#282A36";
+          childBorder = "#282A36";
+        };
+        background    = "#F8F8F2";
+      };
     };
   };
 }
