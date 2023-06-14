9232f60c406a15f1714fe992b43eb00effbfaabc
Guillaume Friloux <gfriloux@galilee.fr>
2023-06-14 16:32:28 +0200

    feat(all): Use tilde var for home paths
    Use tilde var for home paths
    
    Signed-off-by: Guillaume Friloux

--- config/i3.nix
+++ config/i3.nix
@@ -6,6 +6,7 @@ in {
     rofi
     polybar
     i3lock
+    scrot       # Take screenshot for i3lock
     mpv         # For my i3lock script
     imagemagick # To create background for i3lock
     xautolock
@@ -18,17 +19,17 @@ in {
       modifier = "Mod4";
       fonts = ["pango:DejaVu Sans Mono 8"];
       startup = [
-        { notification = false; command = "xautolock -time 10 -detectsleep -locker /home/user/.config/i3/lock.sh"; }
+        { notification = false; command = "xautolock -time 10 -detectsleep -locker ~/.config/i3/lock.sh"; }
         { notification = false; command = "nm-applet"; }
         { notification = false; command = "picom --config ~/.config/picom/picom.conf"; }
-        { notification = false; command = "/home/user/.config/polybar/launch.sh"; }
+        { notification = false; command = "~/.config/polybar/launch.sh"; }
         { notification = false; command = "flameshot"; }
       ];
       keybindings = pkgs.lib.mkOptionDefault {
         "${mod}+Return" = "exec nixGL alacritty";
-        "${mod}+l" = "exec /home/user/.config/i3/lock.sh";
+        "${mod}+l" = "exec ~/.config/i3/lock.sh";
         "${mod}+Shift+q" = "kill";
-        "${mod}+d" = "exec fish -c 'rofi -show drun -theme /home/user/.config/rofi/launchers/type-7/style-1.rasi'";
+        "${mod}+d" = "exec fish -c 'rofi -show drun -theme ~/.config/rofi/launchers/type-7/style-1.rasi'";
       };
       bars = [];
     };
