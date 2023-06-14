5c0ade76ad0c989f67c3e544f42846968e4ce341
Guillaume Friloux <gfriloux@galilee.fr>
2023-06-14 15:10:09 +0200

    feat(all): User external user.nix that stores various config
    User external user.nix that stores various config
    
    Signed-off-by: Guillaume Friloux

--- home.nix
+++ home.nix
@@ -4,43 +4,22 @@
   nixpkgs.config.allowUnfree = true;
 
   imports = [
+    ./user.nix
     ./packages.nix
-    ./config/git.nix
     ./config/fish.nix
     ./config/i3.nix
   ];
-
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "user";
-  home.homeDirectory = "/home/user";
 
   home.stateVersion = "23.05"; # Please read the comment before changing.
   home.keyboard = {
     layout = "fr";
   };
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
 
   fonts.fontconfig.enable = true;
   home.sessionVariables = {
-    EDITOR = "nvim";
+    EDITOR = "micro";
     MICRO_TRUECOLOR=1;
     VISUAL="micro";
-    #LANG="fr_FR.UTF-8";
-    #LC_ALL="fr_FR.UTF-8";
-    #LANGUAGE="fr_FR.UTF-8";
-    #LOCALE_ARCHIVE="$(nix-env --installed --no-name --out-path --query glibc-locales)/lib/locale/locale-archive";
   };
 
   home.language = {
@@ -77,8 +56,6 @@
       '';
     };
   };
-
-  #home.file.".config/alacritty/alacritty.yml".source = ./files/alacritty.yml;
 
   programs = {
     home-manager.enable = true;
